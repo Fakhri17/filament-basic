ad7aa7a68f07bd140416118c0c159ddaabdf45db
Fakhri17 <fakhricoffe@gmail.com>
2024-02-25 10:46:46 +0700

    feat: validation and section group

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -20,6 +20,8 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Group;
 use App\Models\Category;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ImageColumn;
@@ -31,42 +33,58 @@ class PostResource extends Resource
     protected static ?string $model = Post::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-folder';
-    
+
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
-                TextInput::make('title')
-                    ->label('Title')
-                    ->required(),
-                TextInput::make('slug')
-                    ->label('Slug')
-                    ->required(),
-                Select::make('category_id')
-                    ->options(
-                        Category::all()->pluck('name', 'id')
-                    )
-                    ->label('Category'),
 
-                ColorPicker::make('color')
-                    ->label('Color'),
+                Section::make('Content')
+                    ->description('Create a new post')
+                    ->collapsible()
+                    ->schema([
+                        Checkbox::make('is_published')
+                            ->columnSpanFull()
+                            ->label('Is Published'),
+                        TextInput::make('title')
+                            ->rules('min:3|max:255')
+                            ->label('Title')
+                            ->required(),
+                        TextInput::make('slug')
+                            ->label('Slug')
+                            ->unique('posts', 'slug')
+                            ->required(),
+                        Select::make('category_id')
+                            ->options(
+                                Category::all()->pluck('name', 'id')
+                            )
+                            ->label('Category'),
+                        ColorPicker::make('color')
+                            ->label('Color'),
+                        MarkdownEditor::make('content')
+                            ->label('Content')
+                            ->columnSpanFull(),
+                        TagsInput::make('tags')
+                            ->columnSpanFull()
+                            ->label('Tags')
+                    ])->columnSpan(2)->columns(2),
 
-                FileUpload::make('thumbnail')
-                    ->label('Thumbnail')
-                    ->disk('public')
-                    ->directory('thumbnails')
-                    ->image(),
+                Group::make()->schema([
+                    Section::make('Thumbnail')
+                        ->description('Upload a thumbnail for the post')
+                        ->collapsible()
+                        ->schema([
+                            FileUpload::make('thumbnail')
+                                ->label('Thumbnail')
+                                ->image()
+                                ->acceptedFileTypes(['image/png', 'image/jpeg', 'image/jpg'])
+                                ->maxSize(3 * 1024)
+                                ->required(),
+                        ])->columnSpan(1),
+                ]),
 
-                MarkdownEditor::make('content')
-                    ->label('Content'),
-
-                TagsInput::make('tags')
-                    ->label('Tags'),
-                Checkbox::make('is_published')
-                    ->label('Is Published'),
-            ]);
+            ])->columns(3);
     }
 
     public static function table(Table $table): Table
